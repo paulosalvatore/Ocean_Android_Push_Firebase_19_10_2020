7fe77cd5bda10dbe697481b6d2046e912fe09186
Paulo Salvatore <paulo@salvatore.tech>
2020-10-19 13:58:46 -0300

    FirebaseMessagingService criado

--- app/src/main/java/com/oceanbrasil/ocean_android_push_firebase_19_10_2020/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_push_firebase_19_10_2020/MyFirebaseMessagingService.kt
@@ -1,9 +1,36 @@
 package com.oceanbrasil.ocean_android_push_firebase_19_10_2020
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val TAG = "FIREBASE_MESSAGING"
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
-    override fun onNewToken(p0: String) {
-        super.onNewToken(p0)
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // sendRegistrationToServer(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification?.body)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
     }
 }
