424e978a230c9893331e75e01601c1abe3f6bc61
Paulo Salvatore <paulo@salvatore.tech>
2020-10-19 15:46:55 -0300

    Intent adicionada para abrir o app quando a notificação for clicada

--- app/src/main/java/com/oceanbrasil/ocean_android_push_firebase_19_10_2020/NotificationCreator.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_push_firebase_19_10_2020/NotificationCreator.kt
@@ -2,7 +2,9 @@ package com.oceanbrasil.ocean_android_push_firebase_19_10_2020
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 
@@ -37,12 +39,22 @@ object NotificationCreator {
             }
         }
 
+        // Definir qual Activity irá abrir quando clicar na notificação
+
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
         // Criar e enviar a notificação
 
         val builder = NotificationCompat.Builder(context, channelId)
             .setContentTitle(title)
             .setContentText(body)
             .setSmallIcon(android.R.drawable.ic_dialog_alert)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         val notification = builder.build()
         val notificationId = 1
