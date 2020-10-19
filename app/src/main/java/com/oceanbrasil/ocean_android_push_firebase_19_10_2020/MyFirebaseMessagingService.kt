abd7d6bbde655e0f25ec64720cbeeeba5f655511
Paulo Salvatore <paulo@salvatore.tech>
2020-10-19 13:45:48 -0300

    Configuração do Firebase Cloud Messaging finalizada e serviço criado

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_push_firebase_19_10_2020/MyFirebaseMessagingService.kt
@@ -0,0 +1,9 @@
+package com.oceanbrasil.ocean_android_push_firebase_19_10_2020
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+}
